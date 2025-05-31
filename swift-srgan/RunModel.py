fca77c53041a783cb2b83ec1bca924ef01709aa1
Alhad Manure <alhad.7manure@gmail.com>
2025-05-31 12:34:37 +0530

    Fixed resizing issue

--- swift-srgan/RunModel.py
+++ swift-srgan/RunModel.py
@@ -1,21 +1,22 @@
 import torch
 import torchvision.transforms as transforms
 from PIL import Image
+from PIL.Image import Resampling
 import sys
 
 from models import Generator
 
 # ---------- CONFIGURATION ----------
-IMAGE_PATH = 'input.jpg'       # Change to your input image
+IMAGE_PATH = r'D:\\Mtech\\Sem2\\Mini_Project\\SRGAN\\Training1\\Results\\Test1.jpg'       # Change to your input image
 MODEL_PATH = 'model.pt'        # Change to your .pt model path
-OUTPUT_PATH = 'output.jpg'     # Output image path
+OUTPUT_PATH = r'D:\\Mtech\\Sem2\\Mini_Project\\SRGAN\\Training1\\Results\\Test1Output.JPG'     # Output image path
 DEVICE = 'cuda' if torch.cuda.is_available() else 'cpu'
 # -----------------------------------
 
 # 1. Load and preprocess image
 transform_input = transforms.Compose([
-    transforms.ToPILImage(),
-    transforms.Resize((96, 96), interpolation=Image.BICUBIC),
+    #transforms.Resize((96, 96), interpolation=Image.BICUBIC),
+    transforms.Resize(96, interpolation=Resampling.BICUBIC),
     transforms.ToTensor()
 ])
 
@@ -28,8 +29,8 @@ input_tensor = transform_input(image).unsqueeze(0)  # shape: [1, 3, 96, 96]
 # model.eval()
 
 model = Generator()
-checkpoint = torch.load('checkpoint.tar', map_location=DEVICE)
-model.load_state_dict(checkpoint['model_state_dict'])
+checkpoint = torch.load(r'D:\\Mtech\\Sem2\\Mini_Project\\SRGAN\\Training1\\Results\\netG_4x_epoch200.pth.tar', map_location=DEVICE)
+model.load_state_dict(checkpoint['model'])
 model.to(DEVICE)
 model.eval()
 
@@ -44,6 +45,9 @@ with torch.no_grad():
 # 4. Postprocess output
 transform_output = transforms.ToPILImage()
 output_image = transform_output(output_tensor.clamp(0, 1))  # Clamp in case of out-of-range values
+
+# Display the output image
+output_image.show()  
 
 # 5. Save output
 output_image.save(OUTPUT_PATH)
